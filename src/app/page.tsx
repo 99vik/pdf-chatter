cac7656be96acb293aaa052749ee080af1b7216b
Viktor Miric <viktorm47@gmail.com>
2024-02-11 14:43:04 +0100

    Shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+
 export default function Page() {
-  return <p className="">landing page</p>;
+  return (
+    <div>
+      <h1>Interact with your PDF documents instantly</h1>
+      <p>
+        PDF Chatter empowers you to engage with your documents like never
+        before. Simply upload your PDF and delve into dynamic conversations with
+        AI.
+      </p>
+      <Button>
+        <ArrowRight />
+        Button test
+      </Button>
+    </div>
+  );
 }
