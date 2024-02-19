29142309b61b5fe003000b9285b6679aab1b0b74
Viktor Miric <viktorm47@gmail.com>
2024-02-20 00:29:40 +0100

    Dashboard files display styling

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,6 +2,8 @@
 
 import { trpc } from '@/app/_trpc/client';
 import { Button } from './ui/button';
+import { Clock, FileText, Trash } from 'lucide-react';
+import Link from 'next/link';
 
 function File({
   props,
@@ -11,20 +13,30 @@ function File({
     name: string;
   };
 }) {
-  const { name } = props;
-  console.log(name);
+  const { id, name } = props;
   return (
-    <div className="bg-white rounded-lg p-2">
-      <div className="border-b border-zinc-300">
-        <p>{name}</p>
-      </div>
-      <div className="flex justify-between my-1">
-        <p>1 day ago</p>
-        <Button size="sm" variant="destructive">
-          DEL
+    <div className="bg-card rounded-lg py-3 px-3 shadow group transition">
+      <Link
+        href={`/dashboard/${id}`}
+        className="border-b border-neutral-200 flex items-start gap-3 pb-2 hover:text-zinc-500 transition"
+      >
+        <div className="bg-primary h-7 w-7 flex items-center justify-center rounded-full">
+          <FileText className="h-4 w-4 text-white" />
+        </div>
+        <p className="text-lg">{name}</p>
+      </Link>
+      <div className="flex justify-between items-center my-2 px-3 -mb-1">
+        <p className="text-sm text-zinc-500 flex items-center gap-1">
+          <Clock className="h-3 w-3" />1 day ago
+        </p>
+        <Button
+          size="sm"
+          variant="destructive"
+          className="bg-red-100 hover:bg-red-200 w-20 h-8"
+        >
+          <Trash className="h-4 w-4 text-red-700 " />
         </Button>
       </div>
-      <Button className="w-full">Open</Button>
     </div>
   );
 }
