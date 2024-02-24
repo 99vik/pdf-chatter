9f1774ebb9a22dfea61fe4c9d1290ff8016546c3
Viktor Miric <viktorm47@gmail.com>
2024-02-24 23:58:38 +0100

    Page change in toolbar

--- src/components/PdfRender.tsx
+++ src/components/PdfRender.tsx
@@ -5,6 +5,7 @@ import PdfToolbar from './PdfToolbar';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { useResizeDetector } from 'react-resize-detector';
 import SimpleBar from 'simplebar-react';
+import { useState } from 'react';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -15,6 +16,8 @@ export default function PdfRender({
   url: string;
   title: string;
 }) {
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [numPages, setNumPages] = useState<number>();
   const { width, ref } = useResizeDetector();
 
   return (
@@ -22,15 +25,19 @@ export default function PdfRender({
       <h1 className="text-lg my-1 text-center text-zinc-600 font-semibold">
         {title.replace('.pdf', '')}
       </h1>
-      <div className="bg-white flex-1 mx-6 mb-3 rounded-lg border border-zinc-200 shadow-lg flex flex-col">
-        <PdfToolbar />
+      <div className="bg-white flex-1 mx-2 sm:mx-4 lg:mx-6 mb-3 rounded-lg border border-zinc-200 shadow-lg flex flex-col">
+        <PdfToolbar
+          pageNumber={pageNumber}
+          setPageNumber={setPageNumber}
+          numPages={numPages}
+        />
         <SimpleBar
           autoHide={false}
           className="flex-1 max-h-[calc(100vh-61.6px-28px-48.8px-25px)]"
         >
           <div ref={ref} className="flex-1">
             <Document
-              className=""
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
               file={url}
               loading={
                 <div className="bg-white flex justify-center items-center h-96">
@@ -38,7 +45,11 @@ export default function PdfRender({
                 </div>
               }
             >
-              <Page scale={1} width={width ? width : 1} pageNumber={1} />
+              <Page
+                scale={1}
+                width={width ? width : 1}
+                pageNumber={pageNumber}
+              />
             </Document>
           </div>
         </SimpleBar>
--- src/components/PdfToolbar.tsx
+++ src/components/PdfToolbar.tsx
@@ -1,7 +1,50 @@
-export default function PdfToolbar() {
+import { ChevronLeft, ChevronRight, Loader2 } from 'lucide-react';
+import { Dispatch, SetStateAction } from 'react';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+
+export default function PdfToolbar({
+  setPageNumber,
+  pageNumber,
+  numPages,
+}: {
+  setPageNumber: Dispatch<SetStateAction<number>>;
+  pageNumber: number;
+  numPages: number | undefined;
+}) {
   return (
-    <div className="border-b border-zinc-200 p-3">
-      <p>Toolbar</p>
+    <div className="border-b border-zinc-200 h-12 flex items-center px-3">
+      <div className="flex justify-center items-center">
+        <Button
+          disabled={pageNumber <= 1}
+          variant="ghost"
+          size="sm"
+          onClick={() => setPageNumber((prev) => prev - 1)}
+        >
+          <ChevronLeft size={20} />
+        </Button>
+        <div className="flex items-center gap-2">
+          <Input
+            className="w-9 h-8 p-0 text-center"
+            defaultValue={pageNumber}
+          />
+          <p className="flex items-center gap-2">
+            /{' '}
+            {numPages ? (
+              numPages
+            ) : (
+              <Loader2 size={14} className="animate-spin" />
+            )}
+          </p>
+        </div>
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => setPageNumber((prev) => prev + 1)}
+        >
+          <ChevronRight size={20} />
+        </Button>
+      </div>
     </div>
   );
 }
